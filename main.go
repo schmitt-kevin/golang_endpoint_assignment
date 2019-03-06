d88709bf64654388f2d194e03f3f1a91c7d22fcf
schmitt-kevin <schmittkw@yahoo.com>
2019-03-05 22:34:51 -0500

    finished csv import/export

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"encoding/json"
-	//"fmt"
+	// "fmt"
 	"log"
 	"net/http"
+	"encoding/csv"
+	"os"
+	"strconv"
+	"strings"
 
 )
 
@@ -47,37 +51,122 @@ func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-params := mux.Vars(req)
 	var customer Customer
+	lastIdx := len(customers) - 1
+	lastCustomer := customers[lastIdx]
 	_ = json.NewDecoder(req.Body).Decode(&customer)
-	customer.ID = params["id"]
+	lastID, err := strconv.Atoi(lastCustomer.ID)
+	if err != nil {
+		log.Fatalf("Cannot convert str to int: %s", err.Error())
+	}
+	customer.ID = strconv.Itoa(lastID + 1)
 	customers = append(customers, customer)
-	json.NewEncoder(w).Encode(customers)
+	json.NewEncoder(w).Encode(customer)
 }
 
 func EditPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
+	var customer Customer
 	for i, item := range customers {
 		if item.ID == params["id"] {
-			var customer Customer
 			customer.ID = params["id"]
 			_ = json.NewDecoder(req.Body).Decode(&customer)
 			customers[i] = customer
 			break
 		}
 	}
+	json.NewEncoder(w).Encode(customer)
+}
+
+func DownloadAddressBook(w http.ResponseWriter, req *http.Request) {
+	rows := GetRowsFromCustomersJson()
+	f, err := os.Create("ExportAddressBook.csv")
+	if err != nil {
+		log.Fatalf("Cannot create a downloadable address book: %s", err.Error())
+	}
+
+	defer func() {
+		e := f.Close()
+		if e != nil {
+		log.Fatalf("Cannot close created file: %s", err.Error())
+		}
+	}()
+
+	d := csv.NewWriter(f)
+	err = d.WriteAll(rows)
+}
+
+func GetRowsFromCustomersJson() ([][]string) {
+	var rows [][]string
+	for i, item := range customers {
+		var row []string
+		if i == 0 {
+			row = append(row, "First Name")
+			row = append(row, "Last Name")
+			row = append(row, "Phone")
+			row = append(row, "Email")
+			rows = append(rows, row)
+		}
+		row = []string{}
+		row = append(row, item.FirstName)
+		row = append(row, item.LastName)
+		row = append(row, item.Phone)
+		row = append(row, item.Email)
+		rows = append(rows, row)		
+	}
+
+	return rows
+}
+
+func ImportAddressBook(w http.ResponseWriter, req *http.Request) {
+	rows := readCSV("ImportAddressBook.csv")
+	var customer Customer
+	for _, row := range rows {
+		if strings.ToLower(row[0]) != "first name"{
+			customer.FirstName = row[0]
+			customer.LastName = row[1]
+			customer.Phone = row[2]
+			customer.Email = row[3]
+			customer.ID = strconv.Itoa(len(customers) + 1)
+			customers = append(customers, customer)
+			customer = Customer{}
+		}
+	}
 	json.NewEncoder(w).Encode(customers)
+}
+
+func readCSV(name string) [][]string {
+	f, err := os.Open(name)
+	if err != nil {
+		log.Fatalf("Cannot open csv file:%s", err.Error())
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.Comma = ','
+
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Cannot read csv file:%s", err.Error())
+	}
+
+	return rows
 }
 
 func main() {
 	router := mux.NewRouter()
+
 	customers = append(customers, Customer{ID: "1", FirstName: "Kevin", LastName: "Schmitt", Phone: "817-703-9740", Email: "schmittkw@yahoo.com"})
 	customers = append(customers, Customer{ID: "2", FirstName: "Wendy", LastName: "Quest", Phone: "817-303-2195", Email: "wendy_quest@yahoo.com"})
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-	router.HandleFunc("/people/{id}", EditPersonEndpoint).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":1337", router))
+
+	router.HandleFunc("/customer", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/customer/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/customer", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/customer/{id}", DeletePersonEndpoint).Methods("DELETE")
+	router.HandleFunc("/customer/{id}", EditPersonEndpoint).Methods("PUT")
+	router.HandleFunc("/download", DownloadAddressBook).Methods("GET")
+	router.HandleFunc("/import", ImportAddressBook).Methods("GET")
+
+	http.ListenAndServe(":1337", router)
 
 }
